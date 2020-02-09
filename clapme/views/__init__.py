8fdd72001e26ba520fa859fc367b9ed01bc26a17
Kyungju SHIM <roundshoulderim@gmail.com>
2020-02-09 15:09:11 +0900

    Edit init of app, models, views

--- clapme/views/__init__.py
+++ clapme/views/__init__.py
@@ -1,7 +1,6 @@
-# from clapme.__init__ import api
 from flask_restful import Resource
 from .goal import ApiGoal
-from .viewss import ApiUserGoalList, ApiUserGoal, ApiGoalSuccessList, ApiGoalCommentList
+from .viewss import ApiUserGoal, ApiGoalSuccessList, ApiGoalCommentList
 
 
 class HelloWorld(Resource):
@@ -12,7 +11,6 @@ class HelloWorld(Resource):
 def initialize_routes(api):
     api.add_resource(HelloWorld, '/')
     api.add_resource(ApiGoal, '/goal/', '/goal/<int:id>')
-    api.add_resource(ApiUserGoalList, '/user-goal/')
-    api.add_resource(ApiUserGoal, '/user-goal/<int:goal_id>')
+    api.add_resource(ApiUserGoal, '/user-goal/', '/user-goal/<int:goal_id>')
     api.add_resource(ApiGoalSuccessList, '/goal-success/<int:goal_id>')
     api.add_resource(ApiGoalCommentList, '/goal-comment/<int:goal_id>')
