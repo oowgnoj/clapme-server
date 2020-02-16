7eb38a126cbea4cb12d75652786a0024fc79a01d
oowgnoj <mangtaob@gmail.com>
2020-02-17 07:56:46 +0900

    add test_api with some params

--- clapme/test_clapme.py
+++ clapme/test_clapme.py
@@ -10,6 +10,8 @@ sys.path.append(".")
 
 from __init__ import app
 
+
+
 @pytest.fixture
 def client():
     client = app.test_client()
@@ -17,7 +19,7 @@ def client():
     yield client
 
 def test_ApiGoal_post(client):
-    test_data = {"title" :"hello clapme!!!", "description" : "playing!!", "interval" : 5, "times" : 3, "thumbnail":"dnjdl"}
+    test_data = {"title" :"hello clapme!!!", "description" : "playing!!", "interval" : "everyweek", "times" : 3, "thumbnail":"test-thumbnail"}
     rv = client.post('/goal/', data=json.dumps(test_data))
     json_data = rv.get_json()
     assert 200  == rv.status_code
@@ -26,16 +28,23 @@ def test_ApiGoal_get(client):
 
     rv = client.get('/goal/1', data=dict())
     json_data = rv.get_json()
-    answer = {"id": 1, "description": "clapme", "interval": "everyday", "times": 1}
+    answer = {"id": 1, "description": "clapme", "interval": "everyweek", "times": 1}
     assert json_data == answer
 
 
-def test_ApiGoal_patch(client):
-    test_data = {"id" : 1, "title" :"test changed"}
-    
+def test_ApiGoal_patch_with_all_params(client):
+    test_data = {"id" : 1, "title" :"test changed",'description': 'clapme','interval': 'everyweek', 'times': 1, 'thumbnail': None}
     rv = client.patch('/goal/', data=json.dumps(test_data))
     json_data = rv.get_json()
-    answer = {'title': 'test changed', 'description': 'clapme', 'interval': 'everyday', 'times': 1, 'thumbnail': None}
+    answer = {'title': 'test changed', 'description': 'clapme', 'interval': 'everyweek', 'times': 1, 'thumbnail': None}
+    assert json_data == answer
+
+
+def test_ApiGoal_patch_with_some_params(client):
+    test_data = {"id" : 1, "title" :"test changed2"}
+    rv = client.patch('/goal/', data=json.dumps(test_data))
+    json_data = rv.get_json()
+    answer = {'title': 'test changed2', 'description': 'clapme', 'interval': 'everyweek', 'times': 1, 'thumbnail': None}
     assert json_data == answer
 
 
