0d621826714aa7b40a3b7fb6484f7ffb2edf0427
oowgnoj <mangtaob@gmail.com>
2020-02-16 10:47:52 +0900

    edit 기존 api

--- clapme/views/views.py
+++ clapme/views/views.py
@@ -2,7 +2,7 @@ from flask_restful import reqparse, abort, Api, Resource
 from flask import jsonify, request, Flask, make_response
 
 from clapme.models import db, User, UserGoal, Goal, Success, Reaction, Comment
-from clapme.util.helper import to_dict, extract
+from clapme.util.helper import to_dict, extract, extract_if_existed
 from clapme.util.validation import api_json_validator
 from clapme.views.auth import authenticate
 
@@ -30,7 +30,7 @@ class ApiGoal(Resource):
             abort(400, message="{}".format(error))
 
         NewGoal = Goal(description=json_data['description'], title=json_data['title'],
-                       interval=json_data['interval'], times=json_data['times'], thumbnail=json_data['title'])
+                       interval=json_data['interval'], times=json_data['times'], thumbnail=json_data['thumbnail'])
 
         if NewGoal:
             db.session.add(NewGoal)
@@ -40,13 +40,14 @@ class ApiGoal(Resource):
     def patch(self):
         json_data = request.get_json(force=True)
 
-        args = parser.parse_args()
-        request_id = args['id']
-
-        Goal.query.filter_by(id=request_id).update(dict(json_data))
-        db.session.commit() 
+        target = Goal.query.filter_by(id=json_data['id']).first()
+        for item in json_data:
+            if json_data[item] != None:
+                setattr(target, item, json_data[item])
+        db.session.commit()
 
         return '성공적으로 변경되었습니다.'
+
 
     def delete(self):
         args = parser.parse_args()
