b0c10de6e4288cdf735b331468366f8ea48998fc
shimkj <shimkj@dano.me>
2020-07-18 13:53:40 +0900

    Edit schemas

--- clapme/models/__init__.py
+++ clapme/models/__init__.py
@@ -19,7 +19,7 @@ class User(db.Model):
     username = db.Column(db.String(30),
                          index=False,
                          unique=True,
-                         nullable=False)
+                         nullable=False)  # oauth 의 경우 id 를 username 으로 초기설정
     password = db.Column(db.String(30),
                          index=False,
                          unique=False,
@@ -28,82 +28,28 @@ class User(db.Model):
                         index=False,
                         unique=False,
                         nullable=True)
-    profile_pic = db.Column(db.Text,
-                            index=False,
-                            unique=False,
-                            nullable=True)  # null 일 경우 클라이언트에서 초기 이미지로 처리
+    pic_url = db.Column(db.Text,
+                        index=False,
+                        unique=False,
+                        nullable=True)  # null 일 경우 클라이언트에서 초기 이미지로 처리
+    timezone = db.Column(db.String(80),
+                         index=False,
+                         unique=False,
+                         nullable=True)
     admin = db.Column(db.Boolean,
                       index=False,
                       default=False,
                       unique=False,
                       nullable=False)
-    created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    updated = db.Column(db.DateTime, onupdate=datetime.utcnow)
-
-    user_goals = db.relationship(
-        'UserGoal', cascade="all,delete", backref='user', lazy=True)
-    successes = db.relationship(
-        'Success', cascade="all,delete", backref='user', lazy=True)
-    comments = db.relationship(
-        'Comment', cascade="all,delete", backref='user', lazy=True)
-    reactions = db.relationship(
-        'Reaction', cascade="all,delete", backref='user', lazy=True)
-
-
-class UserGoal(db.Model):
-
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer,
-                        db.ForeignKey('user.id'),
-                        nullable=False)
-    goal_id = db.Column(db.Integer,
-                        db.ForeignKey('goal.id'),
-                        nullable=False)
-    is_owner = db.Column(db.Boolean,
-                         default=False,
-                         nullable=False)
-    subscribe = db.Column(db.Boolean,
-                          default=True,
-                          nullable=False)
-    isAccepted = db.Column(db.Boolean,
-                           default=False,
-                           nullable=False)
-    created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    updated = db.Column(db.DateTime, onupdate=datetime.utcnow)
-
-
-class Goal(db.Model):
-
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(30),
-                      index=False,
-                      unique=False,
-                      nullable=False)
-    description = db.Column(db.String(100),
-                            index=False,
-                            unique=False,
-                            nullable=True)
-    interval = db.Column(db.String(30),
-                         index=False,
-                         unique=False,
-                         nullable=False)
-    times = db.Column(db.Integer,
-                      index=False,
-                      unique=False,
-                      nullable=False)
-    thumbnail = db.Column(db.String(100),
-                          index=False,
-                          unique=False,
-                          nullable=True)  # null 일 경우 클라이언트에서 초기 이미지로 처리
-    created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    updated = db.Column(db.DateTime, onupdate=datetime.utcnow)
-
-    user_goals = db.relationship(
-        'UserGoal', cascade="all,delete", backref='goal', lazy=True)
-    routines = db.relationship(
-        'Routine', cascade="all,delete", backref='goal', lazy=True)
-    comments = db.relationship(
-        'Comment', cascade="all,delete", backref='goal', lazy=True)
+    created = db.Column(db.DateTime,
+                        nullable=False,
+                        default=datetime.utcnow)
+    updated = db.Column(db.DateTime,
+                        onupdate=datetime.utcnow)
+    routines = db.relationship('Routine',
+                               cascade="all,delete",
+                               backref='user',
+                               lazy=True)
 
 
 class Routine(db.Model):
@@ -111,11 +57,9 @@ class Routine(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer,
                         db.ForeignKey('user.id'),
+                        index=True,
                         nullable=False)
-    goal_id = db.Column(db.Integer,
-                        db.ForeignKey('goal.id'),
-                        nullable=True)
-    title = db.Column(db.String(30),
+    title = db.Column(db.String(80),
                       nullable=False)
     mon = db.Column(db.Boolean,
                     index=True,
@@ -152,72 +96,88 @@ class Routine(db.Model):
                     unique=False,
                     nullable=False,
                     default=False)
-    time_at = db.Column(db.Integer,
-                        nullable=False)
+    alarm = db.Column(db.Boolean,
+                      index=False,
+                      unique=False,
+                      nullable=False,
+                      default=True),
+    time = db.Column(db.String(30),
+                     unique=False,
+                     nullable=True)
+    category = db.Column(db.String(80),
+                         index=True,
+                         unique=False,
+                         nullable=True)
+    color_id = db.Column(db.Integer,
+                         db.ForeignKey('color.id'),
+                         nullable=False)
     created = db.Column(db.DateTime,
                         nullable=False,
                         default=datetime.utcnow)
-    successes = db.relationship(
-        'Success', cascade="all,delete", backref='routine', lazy=True)
+    updated = db.Column(db.DateTime,
+                        onupdate=datetime.utcnow)
+    successes = db.relationship('Success',
+                                cascade="all,delete",
+                                backref='routine',
+                                lazy=True)
 
 
 class Success(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer,
-                        db.ForeignKey('user.id'),
-                        nullable=False)
     routine_id = db.Column(db.Integer,
                            db.ForeignKey('routine.id'),
                            nullable=False)
-    created = db.Column(db.DateTime,
-                        nullable=False,
-                        default=datetime.utcnow)
-    reactions = db.relationship(
-        'Reaction', cascade="all,delete", backref='success', lazy=True)
-
-
-class Reaction(db.Model):
-
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer,
-                        db.ForeignKey('user.id'),
-                        nullable=False)
-    comment_id = db.Column(db.Integer,
-                           db.ForeignKey('comment.id'),
-                           nullable=True)
-    success_id = db.Column(db.Integer,
-                           db.ForeignKey('success.id'),
-                           nullable=True)
-    type = db.Column(db.String(30),
-                     unique=False,
-                     nullable=False)
-    created = db.Column(db.DateTime,
-                        nullable=False,
-                        default=datetime.utcnow)
-    updated = db.Column(db.DateTime, onupdate=datetime.utcnow)
-
-
-class Comment(db.Model):
-
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer,
-                        db.ForeignKey('user.id'),
-                        nullable=False)
-    goal_id = db.Column(db.Integer,
-                        db.ForeignKey('goal.id'),
-                        nullable=False)
-    contents = db.Column(db.Text,
+    date_str = db.Column(db.String(80),
+                         index=True,
                          unique=False,
                          nullable=False)
-    reactions = db.relationship(
-        'Reaction', cascade="all,delete", backref='comment', lazy=True)
-    created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    updated = db.Column(db.DateTime, onupdate=datetime.utcnow)
+    day = db.Column(db.String(80),
+                    index=True,
+                    unique=False,
+                    nullable=False)
+    created = db.Column(db.DateTime,
+                        nullable=False,
+                        default=datetime.utcnow)
 
 
-class RoutineRecommend(db.Model):
+class Idea(db.Model):
+
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(30),
-                      index=False,
-                      unique=False)
+    title = db.Column(db.String(80),
+                      nullable=False)
+    sub_title = db.Column(db.String(80),
+                          nullable=True)
+    contents = db.Column(db.Text,
+                         nullable=True)
+    pic_url = db.Column(db.Text,
+                        nullable=True)
+    routine_1 = db.Column(db.Text,
+                          nullable=True)
+    routine_2 = db.Column(db.Text,
+                          nullable=True)
+    routine_3 = db.Column(db.Text,
+                          nullable=True)
+    routine_4 = db.Column(db.Text,
+                          nullable=True)
+    routine_5 = db.Column(db.Text,
+                          nullable=True)
+    routine_6 = db.Column(db.Text,
+                          nullable=True)
+    routine_7 = db.Column(db.Text,
+                          nullable=True)
+    routine_8 = db.Column(db.Text,
+                          nullable=True)
+    routine_9 = db.Column(db.Text,
+                          nullable=True)
+
+
+class Color(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    hex_code = db.Column(db.String(30),
+                         index=False,
+                         unique=False)
+    routines = db.relationship('Routine',
+                               cascade="all,delete",
+                               backref='color',
+                               lazy=True)
--- clapme/models/mixin/timestamp_mixin.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-from clapme.__init__ import db
-
-
-class TimestampMixin(object):
-    created = db.Column(
-        db.DateTime, nullable=False, default=datetime.utcnow)
-    updated = db.Column(db.DateTime, onupdate=datetime.utcnow)
